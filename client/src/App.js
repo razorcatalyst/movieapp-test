232d17c01da318ce153d80a16ee6eb965ffa84a1
razorcatalyst <mattyptang@gmail.com>
2018-10-31 12:47:07 +0800

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,35 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Greeting = props => {
-  return <h1>{props.message}</h1>;
-};
-
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      greetings: 'Greeting the state',
-      message: '',
-      movies: [
-        { title: 'I am legend' },
-        { title: 'Avengers' },
-        { title: 'Star Trek' }
-      ]
-    };
-  }
-
   render() {
     return (
       <div className="App">
-        <Greeting message="Another greeting" />
-        <h2>{this.state.greetings}</h2>
-
-        <ul>
-          {this.state.movies.map(movie => {
-            return <li>{movie.title}</li>;
-          })}
-        </ul>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
